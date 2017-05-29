912466430bf70a972156fa5fd59784e81cd16e63
katarzynakich <katarzynakich@gmail.com>
2017-05-29 23:40:16 +0200

    8.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = prompt("Enter triangle base length 'a':");
-var	h = prompt("Enter triangle height 'h':");
+var h = prompt("Enter triangle height 'h':");
 var triangleArea = a*h/2;
 alert("The area of a triangle with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
 console.log("The area of a triangle with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
